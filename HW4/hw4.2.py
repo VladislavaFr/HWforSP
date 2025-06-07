abaa9f644c634924657d17d02c6eee9eab9e835d
VladislavaFn <vladislava.fridmann1@aol.com>
2025-06-07 07:35:40 +0300

    new hw 4.2

--- HW4/hw4.2.py
+++ HW4/hw4.2.py
@@ -3,7 +3,7 @@ user_name = input("Введите свое имя: ")
 correct_answers = 0
 
 questions = ['My name ___ Vova', 'I ___ a coder.', 'I live ___ Moscow.']
-answers = ['is', 'am', 'in']
+answers = ['is', 'am', 'in'] #список верных ответов
 
 print(f"Привет, {user_name}. Предлагаю проверить свои знания английского!")
 user_input_ready = input(f"Наберите 'ready', чтобы начать!")
@@ -11,14 +11,14 @@ if user_input_ready != "ready":
     print("Кажется, вы не хотите играть. Очень жаль.")
 
 elif user_input_ready == "ready":
-    for index, question in enumerate(questions):
+    for index, question in enumerate(questions): #для индексирования вопросов
         quest = input(f"Введи верный ответ: {question}: ")
-        if quest == answers[index]:
+        if quest == answers[index]: #для проверки ответов в списке при помощи срезов
             print("Ответ верный!")
             correct_answers += 1
         else:
             print(f"Неправильно. Правильный ответ: {answers[index]}")
 
-    print(f'Вот и всё! Вы ответили на {correct_answers} вопросов из 3 верно, это {(correct_answers / 3) * 100} процентов.')
+    print(f'Вот и всё! Вы ответили на {correct_answers} вопросов из 3 верно, это {round((correct_answers / 3) * 100), 2} процентов.')
 
 
